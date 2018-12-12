cbdebe72b92a27dca3a4539a6e8256a8f85b4b36
saso78 <sasotrpovski@gmail.com>
2018-12-12 22:51:55 +0100

    Updates

---
+++ precache-manifest.0601e6d2aea9f882bef492dd27042096.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "12a3db8ba7f5423327bb12aed3c41977",
+    "url": "/robots/static/media/SEGA.12a3db8b.woff"
+  },
+  {
+    "revision": "49d69044dcc19f0c7c57",
+    "url": "/robots/static/js/runtime~main.49d69044.js"
+  },
+  {
+    "revision": "d9116b20e827ca373a31",
+    "url": "/robots/static/js/main.d9116b20.chunk.js"
+  },
+  {
+    "revision": "8313db9495e9ba7308e3",
+    "url": "/robots/static/js/1.8313db94.chunk.js"
+  },
+  {
+    "revision": "d9116b20e827ca373a31",
+    "url": "/robots/static/css/main.71834fe5.chunk.css"
+  },
+  {
+    "revision": "8313db9495e9ba7308e3",
+    "url": "/robots/static/css/1.05c4195d.chunk.css"
+  },
+  {
+    "revision": "690ef0109ed17fa016735a988fdb47b7",
+    "url": "/robots/index.html"
+  }
+];
